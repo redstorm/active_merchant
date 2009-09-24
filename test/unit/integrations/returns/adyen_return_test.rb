b96aee62473e97e164c0b1a2d820f7ad1d4017a8
Blake Miller <blake@hobbles.(none)>
2009-09-23 17:02:11 -0700

    Test improvements

--- test/unit/integrations/returns/adyen_return_test.rb
+++ test/unit/integrations/returns/adyen_return_test.rb
@@ -10,10 +10,16 @@ class AdyenReturnTest < Test::Unit::TestCase
   end
   
   def test_failed_return
-    assert true
-    #r = Adyen::Return.new('retcode=-100')
-    #assert_false r.success?
-    #assert_equal HiTrust::Return::CODES['-100'], r.message
+    r = Adyen::Return.new('merchantReference=1234&skinCode=fVmBwBe3&shopperLocale=en_GB&paymentMethod=visa&authResult=REFUSED&pspReference=8612537501807270&merchantSig=%2FPF97ddepNssgRHiWpCEfu1TnxI%3D',
+                          :shared_secret => 'qceaihyyxiducsnczt6bdl7m5z4vao4f')
+    assert_false r.success?
+  end
+  
+  # important: this test ensures that if the digital signature is wrong, the transaction fails
+  def test_bad_signature
+    r = Adyen::Return.new('merchantReference=1234&skinCode=fVmBwBe3&shopperLocale=en_GB&paymentMethod=visa&authResult=AUTHORISED&pspReference=8612537486987238&merchantSig=6T6mc7NDB1c5I6So816B2hYMNRY%3D',
+                          :shared_secret => 'QCEAIHYyxiducsnczt6bdl7m5z4vao4f') # NOTE this is intentionally the wrong shared secret, which should produce a signature verification failure
+    assert_false r.success?
   end
   
 end
